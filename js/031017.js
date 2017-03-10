b3c33049d6ad4388f82929302ba7f330bf975919
Nathan Stuart <nathan.stuart01@gmail.com>
2017-03-10 07:34:18 -0700

    js practice for 031017 added

---
+++ js/031017.js
@@ -0,0 +1,19 @@
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function() {
+    return this.rooms - this.booked;
+  };
+}
+
+
+var quayHotel = new Hotel('Quay', 40, 25);
+
+
+
+var details1 = quayHotel.name + ' rooms: ';
+    details1 += quayHotel.checkAvailability();
+
+var elHotel1 = document.getElementById('hotel1');
+elHotel1.textContent = details1;
