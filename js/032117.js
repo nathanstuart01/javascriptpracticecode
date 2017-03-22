b29ac8b4cbf733d752aa2753636f055907e69b89
Nathan Stuart <nathan.stuart01@gmail.com>
2017-03-22 07:12:56 -0600

    ways to select different nodes within the DOM tree

--- js/032117.js
+++ js/032117.js
@@ -1,10 +1,31 @@
-let el = document.getElementById('one');
+//let el = document.getElementById('one');
 
-el.className ='cool';
+//el.className ='cool';
 
-let elements = document.getElementsByClassName('hot');
+//let elements = document.getElementsByTagName('li');
+//
+//if (elements.length > 0 ) {
+//  let el = elements[0];
+//  el.className = 'cool';
+//}
 
-if (elements.length > 2) {
-  let el = elements[2];
-  el.className = 'cool';
-}
+//let el = document.querySelector('li.hot');
+//el.className = 'cool';
+
+//let els = document.querySelectorAll('li.hot');
+//els[1].className = 'cool';
+
+//var hotItems = document.querySelectorAll('li.hot');
+
+//if (hotItems.length > 0) {
+//  for (var i = 0; i<hotItems.length; i++) {
+//    hotItems[i].className = 'cool';
+//  }
+//}
+
+let startItem = document.getElementById('two');
+let prevItem = startItem.previousSibling;
+let nextItem = startItem.nextSibling;
+
+prevItem.className = 'complete';
+nextItem.className = 'cool';
