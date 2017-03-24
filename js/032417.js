6d060367aac6ce3769bb43dd831b223bced1dc68
Nathan Stuart <nathan.stuart01@gmail.com>
2017-03-24 07:01:17 -0600

    practiced more DOM manipulation methods

---
+++ js/032417.js
@@ -0,0 +1,27 @@
+//let newEl = document.createElement('li');
+
+//let newText = document.createTextNode('quinoa');
+
+//newEl.appendChild(newText);
+
+//let position = document.getElementsByTagName('ul')[0];
+
+//position.appendChild(newEl);
+
+//let removeEl = document.getElementsByTagName('li')[3];
+//let containerEl = removeEl.parentNode;
+let firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+let fourthItem = document.getElementsByTagName('li').item(3);
+fourthItem.setAttribute('class', 'cool');
+
+//if (firstItem.hasAttribute('class')) {
+  //let attr = firstItem.getAttribute('class');
+
+//  let el = document.getElementById('scriptResults');
+//  el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
+
+//}
+
+//containerEl.removeChild(removeEl);
