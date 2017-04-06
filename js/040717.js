ae28142b59989308ac1a2c7ff99a0de28265fa40
Nathan Stuart <nathan.stuart01@gmail.com>
2017-04-06 07:27:30 -0600

    practiced event listeners with forms and word counters in textareas, not complete form example

---
+++ js/040717.js
@@ -0,0 +1,14 @@
+let el;
+
+function charCount(e) {
+let textEntered, charDisplay, counter, lastkey;
+textEntered = document.getElementById('message').value;
+charDisplay = document.getElementById('charactersLeft');
+counter = (180 - (textEntered.length));
+charDisplay.textContent = counter;
+
+lastkey = document.getElementById('lastkey');
+lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode;
+}
+el = document.getElementById('message');
+el.addEventListener('keyup', charCount, false);
