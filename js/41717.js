c9447580be132b291e2e3bf942f23767c77f42f8
Nathan Stuart <nathan.stuart01@gmail.com>
2017-04-14 21:55:25 -0600

    practiced multiple jquery examples of methods that remove and update dom elements

---
+++ js/41717.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+//  $(function() {
+//    let $window = $(window);
+//let $slideAd = $('#slideAd');
+//  //  let endZone = $('#footer').offset().top - $window.height();
+//
+//    $window.on('scroll', function() {
+//      if ( (endZone) < $window.scrollTop() ) {
+//        $slideAd.animate({ 'right': 'Opx' }, 250);
+//      } else {
+//        $slideAd.stop(true).animate({ 'right': '-360px' }, 250);
+//      }
+//
+//    });
+//  });
+
+});
